6082b6a5782d9d1eb77c5ff049486e1fd8a3d529
karolkozak <karol2868@gmail.com>
2018-02-23 08:46:56 +0100

    UNNAMED-WEB-5 | Provide notifications, notify error responses

--- src/app/shared/interceptors/global-exception-interceptor.ts
+++ src/app/shared/interceptors/global-exception-interceptor.ts
@@ -1,17 +1,20 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
+import {NotificationsService} from 'angular2-notifications';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class GlobalExceptionInterceptor implements HttpInterceptor {
 
+  constructor(private notificationService: NotificationsService) {
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req)
       .catch((error: any) => {
-        console.error(error.status);
-        console.error(error.error.message);
+        this.notificationService.error(`Exception - ${error.status}`, error.error.message, {timeOut: 100000});
         return Observable.throw(error);
       }) as any;
   }
