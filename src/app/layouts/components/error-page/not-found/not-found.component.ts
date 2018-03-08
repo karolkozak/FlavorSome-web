dcbe5a58826dd3620cb6c229e27beff2bc920168
Herioz <kmacieslik@gmail.com>
2018-03-08 20:43:16 +0100

    [Unnamed-web-3] Fixes based on review.

--- src/app/layouts/components/error-page/not-found/not-found.component.ts
+++ src/app/layouts/components/error-page/not-found/not-found.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
@@ -6,14 +6,7 @@ import {Router} from '@angular/router';
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent implements OnInit {
-  public router: Router;
+export class NotFoundComponent {
 
-  constructor(private _router: Router) {
-    this.router = _router;
-  }
-
-  ngOnInit() {
-  }
-
+  constructor(public router: Router) {}
 }
