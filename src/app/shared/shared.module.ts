9b3bc5281ecbaf36acb02c3eb210be22f4f02902
kkopec <kopeckrzysztoff@gmail.com>
2018-03-10 18:06:33 +0100

    Unnamed-web-10 | Add styles for buttons
    Applied material design directive to all existing buttons.
    Reordered some imports to follow the convention.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,17 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {ConfigService} from './services/config.service';
 import {HttpClientModule} from '@angular/common/http';
+import {MatButtonModule} from '@angular/material';
 import {httpInterceptorProviders} from './interceptors/http-interceptors';
+import {ConfigService} from './services/config.service';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule,
+    MatButtonModule,
   ],
+  exports: [MatButtonModule],
   providers: [ConfigService, httpInterceptorProviders],
   declarations: []
 })
