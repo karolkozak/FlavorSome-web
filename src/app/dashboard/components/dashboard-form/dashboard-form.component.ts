a696eb79a8e5a88127efdb4cd764f2cbf252b072
kkopec <kopeckrzysztoff@gmail.com>
2018-03-30 23:41:32 +0200

    Add dashboard form
    Added dashboard form definition.
    Configured grid for a dashboard layout part.

--- src/app/dashboard/components/dashboard-form/dashboard-form.component.ts
+++ src/app/dashboard/components/dashboard-form/dashboard-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'un-dashboard-form',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-form.component.scss']
 })
 export class DashboardFormComponent implements OnInit {
+  placesSearchForm: FormGroup;
+  placeTypes = [
+    {name: 'Restaurant'},
+    {name: 'Cafe'},
+    {name: 'Pub'},
+    {name: 'FastFood'},
+  ];
+  price = 50;
+  rating = 100;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.placesSearchForm = this.formBuilder.group({
+      location: [''],
+      range: ['', Validators.compose([
+        Validators.min(10),
+        Validators.max(10000),
+      ])],
+      placeType: [''],
+      preferences: [''],
+      price: [''],
+      rating: ['']
+    });
   }
 
+  findPlaces() {
+    return null;
+  }
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { DashboardMapComponent } from './components/dashboard-map/dashboard-map.component';
 import { DashboardFormComponent } from './components/dashboard-form/dashboard-form.component';
 import {AgmCoreModule} from '@agm/core';
+import {SharedModule} from '@app/shared/shared.module';
 
 @NgModule({
   imports: [
     AgmCoreModule,
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   declarations: [DashboardMapComponent, DashboardFormComponent],
   exports: [DashboardMapComponent, DashboardFormComponent],
