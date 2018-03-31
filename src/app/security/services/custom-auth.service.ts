1e0a6e5508175c2f658dcf112f35a3fabcdd4845
karolkozak <karol2868@gmail.com>
2018-03-31 23:56:00 +0200

    Unnamed-web-12 | Add userNotFound notification after failed login

--- src/app/security/services/custom-auth.service.ts
+++ src/app/security/services/custom-auth.service.ts
@@ -1,39 +1,28 @@
 import {Injectable} from '@angular/core';
 
 import {AuthenticationService} from './authentication.service';
-import {environment} from '../../../environments/environment';
+import {environment} from '@env/environment';
 import {HttpClient} from '@angular/common/http';
-import {NotificationsService} from 'angular2-notifications';
-import {CustomTranslateService} from '../../shared/services/custom-translate.service';
 import {Router} from '@angular/router';
+import {tap} from 'rxjs/operators';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class CustomAuthService extends AuthenticationService {
 
   private baseUrl: string;
-  private titleMessage: string;
-  private errorMessage: string;
 
-  constructor(router: Router,
-              private httpClient: HttpClient,
-              private notificationsService: NotificationsService,
-              private customTranslateService: CustomTranslateService) {
+  constructor(router: Router, private httpClient: HttpClient) {
     super(router);
     this.baseUrl = environment.unnamedMicroserviceUrl + environment.authPath;
   }
 
-  login(userData: any): Promise<boolean> {
+  login(userData: any): Observable<string> {
     const endpoint = this.baseUrl + environment.loginPath;
-    return this.httpClient.post<string>(endpoint, userData, {responseType: 'text'} as any as {}).toPromise().then(response => {
-      this.setTokenDataInStorage(response);
-      return true;
-    }).catch(error => {
-      console.error(error);
-      this.customTranslateService.getTranslation('Log in').subscribe(result => this.titleMessage = result);
-      this.customTranslateService.getTranslation('Unable to log in, try again later')
-        .subscribe(result => this.errorMessage = result);
-      this.notificationsService.error(this.titleMessage, this.errorMessage);
-      return false;
-    });
+    return this.httpClient.post<string>(endpoint, userData, {responseType: 'text'} as any as {}).pipe(
+      tap(response => {
+        this.setTokenDataInStorage(response);
+      })
+    );
   }
 }
