21e79b2bcdc38ec5251a5acc3cb1b992357ac93e
kkopec <kopeckrzysztoff@gmail.com>
2018-09-09 01:27:17 +0200

    Move user localizing to service

--- src/app/dashboard/components/dashboard-form/dashboard-form.component.ts
+++ src/app/dashboard/components/dashboard-form/dashboard-form.component.ts
@@ -3,8 +3,6 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ConfigService} from '@app/core/services/config.service';
 import {Observable} from 'rxjs/Observable';
 import {PlacesSearchService} from '@app/dashboard/services/places-search.service';
-import {ToastrService} from 'ngx-toastr';
-import {CustomTranslateService} from '@app/core/services/custom-translate.service';
 
 @Component({
   selector: 'un-dashboard-form',
@@ -14,14 +12,12 @@ import {CustomTranslateService} from '@app/core/services/custom-translate.servic
 export class DashboardFormComponent implements OnInit {
   private static RANGE = 300;
 
-  isLocationDisabled = true;
+  isLocationDisabled = !navigator.geolocation;
   placeTypes: Observable<string[]>;
   placesSearchForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder,
               private configService: ConfigService,
-              private toastr: ToastrService,
-              private customTranslateService: CustomTranslateService,
               private placesService: PlacesSearchService) { }
 
   ngOnInit() {
@@ -39,38 +35,17 @@ export class DashboardFormComponent implements OnInit {
     // TODO: add form errors
 
     this.placesService.setSearchRadius(DashboardFormComponent.RANGE);
-
-    if (!!navigator.geolocation) {
-      this.isLocationDisabled = false;
-    }
   }
 
   useBounds(): void {
     this.placesService.resetUserPosition();
   }
 
-  locateUser($event): void {
-    navigator.geolocation.getCurrentPosition(position => {
-      const {latitude, longitude} = position.coords;
-      this.placesService.setUserPosition(latitude, longitude);
-    }, error => {
-      switch (error.code) {
-        case error.PERMISSION_DENIED:
-          return;
-        case error.POSITION_UNAVAILABLE:
-        case error.TIMEOUT:
-        default:
-          let [errorTitle, errorMessage] = ['', ''];
-          this.customTranslateService.getTranslation('An error occurred')
-            .subscribe(result => errorTitle = result);
-          this.customTranslateService.getTranslation('Could not fetch the location')
-            .subscribe(result => errorMessage = result);
-          this.toastr.error(errorMessage, errorTitle);
-      }
-    });
+  locateUser(): void {
+    this.placesService.locateUser();
   }
 
-  updateRange($event): void {
+  updateRange(): void {
     // TODO: add debounce?
     const rangeControl = this.placesSearchForm.get('range');
     if (rangeControl.valid) {
--- src/app/dashboard/services/places-search.service.ts
+++ src/app/dashboard/services/places-search.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {HttpClient} from '@angular/common/http';
-import {filter} from 'rxjs/operators';
 import {environment} from '@env/environment';
+import {CustomTranslateService} from '@app/core/services/custom-translate.service';
+import {ToastrService} from 'ngx-toastr';
 
 @Injectable()
 export class PlacesSearchService {
@@ -10,16 +11,35 @@ export class PlacesSearchService {
   userPosition: BehaviorSubject<google.maps.LatLng> = new BehaviorSubject(undefined);
   searchRadius: BehaviorSubject<number> = new BehaviorSubject(undefined);
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private toastr: ToastrService,
+              private customTranslateService: CustomTranslateService) { }
 
   setSearchRadius(radius: number): void {
     this.searchRadius.next(radius);
   }
 
-  setUserPosition(lat: number, lng: number): void {
-    const position = new google.maps.LatLng(lat, lng);
-    this.userPosition.next(position);
-    this.map.value.panTo(position);
+  locateUser(): void {
+    navigator.geolocation.getCurrentPosition(position => {
+      const {latitude: lat, longitude: lng} = position.coords;
+      const latlng = new google.maps.LatLng(lat, lng);
+      this.userPosition.next(latlng);
+      this.map.value.panTo(latlng);
+    }, error => {
+      switch (error.code) {
+        case error.PERMISSION_DENIED:
+          return;
+        case error.POSITION_UNAVAILABLE:
+        case error.TIMEOUT:
+        default:
+          let [errorTitle, errorMessage] = ['', ''];
+          this.customTranslateService.getTranslation('An error occurred')
+            .subscribe(result => errorTitle = result);
+          this.customTranslateService.getTranslation('Could not fetch the location')
+            .subscribe(result => errorMessage = result);
+          this.toastr.error(errorMessage, errorTitle);
+      }
+    });
   }
 
   resetUserPosition(): void {
@@ -27,14 +47,7 @@ export class PlacesSearchService {
   }
 
   getPlaces() {
-    // TODO: build API call params
-    this.http.get<any>(environment.googlePlacesUrl).pipe(
-      filter(this.filterVisible)
-      // TODO: add respose handling
-    );
-  }
-
-  private filterVisible(place1, place2): boolean {
-    return true; // FIXME: method stub
+    this.http.get<any>(environment.googlePlacesUrl);
+    // FIXME: method stub
   }
 }
