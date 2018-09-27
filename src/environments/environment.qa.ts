370cd8fa4d9d623e7c5d99688c30b15f40ef25bf
karolkozak <karol2868@gmail.com>
2018-09-27 13:56:12 +0200

    [NT] Add qa environments

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,43 @@
+export const environment = {
+  production: false,
+
+  unnamedMicroserviceUrl: 'http://ghost.kkopec.net:8080/api',
+  authPath: '/auth',
+  facebookPath: '/facebook',
+  loginPath: '/login',
+  logoutPath: '/logout',
+  registration: '/register',
+  confirmation: '/confirmation',
+
+  configPath: '/config',
+  placeTypesPath: '/places/types',
+
+  placesPath: '/places',
+  menuPath: '/menu',
+  ratingsPath: '/ratings',
+
+  usersPath: '/users',
+  current: '/current',
+  unrated: '/unrated',
+  visit: '/visit',
+
+  availableLanguages: ['pl', 'en'],
+  availableCultureLanguages: ['pl-PL', 'en-GB'],
+  defaultLanguage: 'en',
+
+  facebookAppId: '209464289792135',
+  facebookSecret: 'f47826dc3bcbbafa2052b1c5f91b157f',
+
+  googleApiKey: 'AIzaSyC9sBR6fDjfqbKDPJ_AX8y45G9L4E7pFOg',
+  googleReCaptchaKey: '6LfaQlAUAAAAAC2husiZGVe0gCLEGiNyt71svf4Y',
+
+  googlePlacesUrl: 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json',
+
+  mapDefaults: {
+    coords: {
+      lat: 50.0617446,
+      lng: 19.948338
+    },
+    zoom: 14
+  },
+};
