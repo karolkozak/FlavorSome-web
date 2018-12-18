92fc46ffb63de71053adfacb082535ffb073ce52
kkopec <kopeckrzysztoff@gmail.com>
2018-12-19 00:22:50 +0100

    #107 | Add MapService and PlacesSearchService providers in dashboard component
    Providing services with component injector should allow for using multiple instances of components without services interferences.

--- src/app/layouts/components/dashboard/dashboard.component.ts
+++ src/app/layouts/components/dashboard/dashboard.component.ts
@@ -1,10 +1,17 @@
 import {Component, OnInit} from '@angular/core';
 import {CONST_TITLES, CustomTitleService} from '@app/core/services/custom-title.service';
+import {MapService} from '@app/core/services/map/map.service';
+import {HereMapService} from '@app/core/services/map/here-map.service';
+import {PlacesSearchService} from '@app/dashboard/services/places-search.service';
 
 @Component({
   selector: 'fs-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  providers: [
+    {provide: MapService, useClass: HereMapService},
+    PlacesSearchService
+  ]
 })
 export class DashboardComponent implements OnInit {
   constructor(private customTitleService: CustomTitleService) {
