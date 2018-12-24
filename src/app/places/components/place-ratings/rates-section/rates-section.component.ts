a32789cb81c011ba3992c2bcd8e801cfcde40289
karolkozak <karol2868@gmail.com>
2018-12-24 14:36:07 +0100

    [#156] Forbid unverify user to rate place

--- src/app/places/components/place-ratings/rates-section/rates-section.component.ts
+++ src/app/places/components/place-ratings/rates-section/rates-section.component.ts
@@ -1,21 +1,32 @@
-import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {PlacesService} from '@app/places/services/places.service';
 import {Rate} from '@app/places/models/rate';
 import {AuthenticationService} from '@app/security/services/authentication.service';
 import {Pageable} from '@app/places/models/pageable';
 import {PageableParams} from '@app/places/models/pageable-params';
+import {UserService} from '@app/core/services/user.service';
+import {User, UserRole} from '@app/security/models/user';
 
 @Component({
   selector: 'fs-rates-section',
   templateUrl: './rates-section.component.html',
   styleUrls: ['./rates-section.component.scss']
 })
-export class RatesSectionComponent implements OnChanges {
+export class RatesSectionComponent implements OnInit, OnChanges {
   @Input() placeId: string;
   pageableRates: Pageable<Rate>;
   pageableParams: PageableParams = new PageableParams();
+  private currentUser: User;
 
-  constructor(private placesService: PlacesService, private authenticationService: AuthenticationService) {
+  constructor(private placesService: PlacesService,
+              private authenticationService: AuthenticationService,
+              private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe(user => {
+      this.currentUser = user;
+    });
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -36,5 +47,9 @@ export class RatesSectionComponent implements OnChanges {
 
   public isLoggedIn(): boolean {
     return this.authenticationService.isLoggedIn();
+  }
+
+  public hasRequiredRole(): boolean {
+    return this.currentUser.role !== UserRole.UNVERIFIED;
   }
 }
