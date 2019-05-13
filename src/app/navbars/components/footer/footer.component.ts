16f96291f300af90e0a3de12bf84905b8c5085ae
Karol Kozak <karol2868@gmail.com>
2019-05-13 11:54:01 +0200

    [NT] Add year range to footer

--- src/app/navbars/components/footer/footer.component.ts
+++ src/app/navbars/components/footer/footer.component.ts
@@ -1,9 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'fs-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  currentYear: string;
+  ngOnInit(): void {
+    this.currentYear = moment().format('YYYY');
+  }
 }
